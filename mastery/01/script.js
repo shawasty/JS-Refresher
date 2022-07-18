d6a17aa5a539b6b00b6f640833ec144b798b53b4
shawasty <shawasty@gmail.com>
2022-07-18 02:08:01 -0400

    commits

--- mastery/01/script.js
+++ mastery/01/script.js
@@ -1,1 +1,16 @@
-console.log("Nana Amankwah")
+// let varName = "Welcome to Variables Nana";
+
+// console.log(varName);
+
+// let num = `${2 + 2}`
+// let num1 = 2 + 2
+
+// console.log(typeof num1)
+
+// let isCool = false;
+
+// if(isCool){
+//     console.log("hi there, you are cool")
+// } else {
+//     console.log(" That's it .. ")
+// }
