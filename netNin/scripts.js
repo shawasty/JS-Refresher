f9118eb348f6d4d72f461345436ba81ecc983d4c
shawasty <shawasty@gmail.com>
2022-07-25 03:29:32 -0400

    return statement

--- netNin/scripts.js
+++ netNin/scripts.js
@@ -1,13 +1,21 @@
 
 
-speak("Hilda","morning");
+// speak("Hilda","morning");
 
-const answer = function (ques){
-    console.log(`whatg is ${ques}`)
+// const answer = function (ques){
+//     console.log(`whatg is ${ques}`)
+// }
+
+
+
+// function speak(name,time){
+//     console.log(`Hello ${name} ${time}`)
+// }
+
+const calcArea = function(radius){
+        let area = 3.14 * radius**2
+        return area;
 }
 
-
-
-function speak(name,time){
-    console.log(`Hello ${name} ${time}`)
-}
+const a = calcArea(5)
+console.log(a)
