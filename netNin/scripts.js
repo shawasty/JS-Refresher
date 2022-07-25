a3be863005fba48818ff42c2a90d29d2f9cc3785
shawasty <shawasty@gmail.com>
2022-07-25 03:23:33 -0400

    netnin' git push

---
+++ netNin/scripts.js
@@ -0,0 +1,13 @@
+
+
+speak("Hilda","morning");
+
+const answer = function (ques){
+    console.log(`whatg is ${ques}`)
+}
+
+
+
+function speak(name,time){
+    console.log(`Hello ${name} ${time}`)
+}
